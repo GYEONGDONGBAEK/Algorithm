a467031eb77e84c5b860332b35757f1d701a4023
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-04-16 20:40:25 +0900

    Time: 1 ms (86.27%), Space: 40.5 MB (19.97%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.java
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.java
@@ -3,7 +3,7 @@ class Solution {
         int answer=0;
         for(int i=0;i<nums.length;i++){
             for(int j=i+1;j<nums.length;j++){
-                if(nums[i]==nums[j]&&i<j) answer++;
+                if(nums[i]==nums[j]) answer++;
             }
         }
         return answer;
