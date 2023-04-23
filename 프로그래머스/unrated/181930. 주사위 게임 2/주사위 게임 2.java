6be1d0f3b02341b3d444676da6963ca912e313d9
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-04-23 22:20:12 +0900

    [unrated] Title: 주사위 게임 2, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int a, int b, int c) {
+        int answer=0;
+        if (a!=b&&b!=c&&a!=c){
+            answer=a+b+c;
+        }
+        if(a==b&&b!=c){
+            answer=(a+b+c)*(a*a+b*b+c*c);
+        }else if(b==c&&c!=a){
+            answer=(a+b+c)*(a*a+b*b+c*c);
+        }else if(c==a&&a!=b){
+            answer=(a+b+c)*(a*a+b*b+c*c);
+        }
+        if(a==b&&b==c){
+            answer=(a+b+c)*(a*a+b*b+c*c)*(a*a*a+b*b*b+c*c*c);
+        }
+        return answer;
+    }
+}
