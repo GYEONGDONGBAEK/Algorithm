826920e69593c8e90d40d1542b3ed722c6aa684b
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-04-24 16:08:55 +0900

    [level 2] Title: 올바른 괄호, Time: 6.98 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,16 @@
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        if(s.charAt(0)==')') answer= false;
+        
+        int cnt=0;
+        for (int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if (c=='(') cnt++;
+            else cnt--;
+            if(cnt<0) answer=false;
+        }
+        if(cnt>0) answer=false;
+        return answer;
+    }
+}
