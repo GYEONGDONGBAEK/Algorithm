8f605b5d3d455caa9ae541800d62407eefcb0836
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-04-27 20:25:46 +0900

    [unrated] Title: 글자 지우기, Time: 2.28 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        String [] str=my_string.split("");
+        String answer="";
+        for(int i=0;i<indices.length;i++){
+            str[indices[i]]="";
+        }
+        for(String s:str){
+            answer+=s;
+        }
+        return answer;
+    }
+}
