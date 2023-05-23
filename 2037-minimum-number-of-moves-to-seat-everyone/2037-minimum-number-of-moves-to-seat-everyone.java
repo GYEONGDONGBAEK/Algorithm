cdcdb88b8c52270359c96e8b493a20a87481364b
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-05-23 19:59:11 +0900

    Time: 3 ms (22.19%), Space: 43.3 MB (5.68%) - LeetHub

---
+++ 2037-minimum-number-of-moves-to-seat-everyone/2037-minimum-number-of-moves-to-seat-everyone.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Solution {
+    public int minMovesToSeat(int[] seats, int[] students) {
+        Arrays.sort(seats);
+        Arrays.sort(students);
+        int ans=0;
+        for(int i=0;i<seats.length;i++){
+            ans+=Math.abs(students[i]-seats[i]);
+        }
+        return ans;
+    }
+}
