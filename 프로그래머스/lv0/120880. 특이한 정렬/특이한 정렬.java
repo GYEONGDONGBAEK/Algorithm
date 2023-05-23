21f7655fb5a96cdc2eab87288ffb90e05123b3f4
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-05-23 12:56:00 +0900

    [level 0] Title: 특이한 정렬, Time: 0.46 ms, Memory: 70.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,33 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        HashMap<Integer, Integer> map=new HashMap<>();
+        for (int num : numlist){
+            int gap=Math.abs(num-n);
+            map.put(num,gap);
+        }
+        Integer[] numArray=new Integer[numlist.length];
+        for (int i=0;i<numlist.length;i++) {
+            numArray[i] = numlist[i];
+        }
+        Arrays.sort(numArray, new Comparator<Integer>() {
+            public int compare(Integer a, Integer b) {
+                int gapA=map.get(a);
+                int gapB=map.get(b);
+        
+                if (gapA==gapB) {
+                    return Integer.compare(b,a);
+                } else {
+                    return Integer.compare(gapA,gapB);
+                }
+            }
+        });
+        
+        int[] result = new int[numlist.length];
+        for (int i=0;i<numlist.length;i++) {
+            result[i] = numArray[i];
+        }
+        
+        return result;
+    }
+}
