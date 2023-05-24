c299069aa10850a7543dee454d6532f393036038
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-05-24 17:43:27 +0900

    [unrated] Title: 접미사 배열, Time: 0.24 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        int length=my_string.length();
+        String[] suffixes=new String[length];
+
+        for (int i=0;i<length;i++) {
+            suffixes[i]=my_string.substring(i,length);
+        }
+        Arrays.sort(suffixes);
+        return suffixes;
+    }
+}
