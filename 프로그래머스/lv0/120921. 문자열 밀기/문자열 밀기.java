32ed8a98de327f1fda475be0281a8c1c08bdbd15
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-05-25 17:22:41 +0900

    [level 0] Title: 문자열 밀기, Time: 1.25 ms, Memory: 71.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.java
@@ -3,7 +3,7 @@ class Solution {
         if (A.length()!=B.length()) return -1;
         if (A.equals(B)) return 0;
         int a=A.length();
-        for (int i=0;i<a;i++) {
+        for (int i=1;i<a;i++) {
             String str=A.substring(a-i)+A.substring(0,a-i);
             if (str.equals(B)) return i; 
         }
