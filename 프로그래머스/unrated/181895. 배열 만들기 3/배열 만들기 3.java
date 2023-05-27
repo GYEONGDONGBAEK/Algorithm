4d17896cf232432ea730aa89d2b96c115f950ba7
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-05-27 23:14:56 +0900

    [unrated] Title: 배열 만들기 3, Time: 0.16 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181895. 배열 만들기 3/배열 만들기 3.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int[] arr, int[][] intervals) {
+        int[] result = new int[(intervals[0][1]-intervals[0][0]+1)+(intervals[1][1]-intervals[1][0]+1)];
+        int index=0;
+        for (int i=intervals[0][0];i<=intervals[0][1];i++) result[index++]=arr[i];
+        for (int i=intervals[1][0];i<=intervals[1][1];i++) result[index++]=arr[i];
+        return result;
+    }
+}
