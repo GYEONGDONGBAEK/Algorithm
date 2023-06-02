76fe58033a9a54371df8036a5b6acc08230a1154
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-06-02 19:27:30 +0900

    [unrated] Title: 추억 점수, Time: 0.04 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] name, int[] yearning, String[][] photo) {
+        Map<String,Integer> map=new HashMap<>();
+        
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+        int[] answer = new int[photo.length];
+        for(int i=0;i<photo.length;i++){
+            for(int j=0;j<photo[i].length;j++){
+                String person = photo[i][j];
+                if (map.containsKey(person)) answer[i]+=map.get(person);
+            
+            }
+        }
+        return answer;
+    }
+}
+
+
+/*
+ for(int i=0;i<photo.length;i++){
+            for(int j=0;j<photo[i].length;j++){
+                answer[i]+=map.get(photo[i][j]); <- nullpointException 발생
+            }
+        }
+*/
