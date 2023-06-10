8c39eee04fa8038985af11bcd458000120ec6119
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-06-10 20:01:24 +0900

    [unrated] Title: 귤 고르기, Time: 32.94 ms, Memory: 114 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int size=0;
+        for (int t:tangerine) {
+            size=Math.max(t, size);
+        }
+        
+        int[] cnt=new int[size+1];  //크기별 귤의 개수 저장하는 배열
+        
+        for (int i=0;i<tangerine.length;i++) {
+            cnt[tangerine[i]]+=1;  //인덱스가 귤의 크기
+        }
+        Arrays.sort(cnt);
+        int sum=0;
+        int count=1;
+        
+        for (int i=size;i>=1;i--) {
+            sum += cnt[i];
+            
+            if (sum>=k) {
+	             break;
+            }
+            count++;
+        }
+        return count;
+    }
+}
