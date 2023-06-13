9dedcfcf464c0ece092c7eff1e0e3c4d58297eb2
GyeongDongBaek <122242439+GYEONGDONGBAEK@users.noreply.github.com>
2023-06-13 23:45:59 +0900

    Time: 3 ms (38.54%), Space: 44.2 MB (15.07%) - LeetHub

---
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode constructMaximumBinaryTree(int[] nums) {
+    
+         return BTree(nums,0,nums.length-1);
+    }
+    
+    private TreeNode BTree(int[] nums, int start, int end) {
+        if (start>end) return null;
+        
+
+        int max=Integer.MIN_VALUE;
+        int maxindex=0;
+        for (int i=start;i<=end;i++) {
+            if (nums[i]>max) {
+                max=nums[i];
+                maxindex=i;
+            }
+        }
+
+        TreeNode root = new TreeNode(max);
+        root.left = BTree(nums, start, maxindex-1);
+        root.right = BTree(nums, maxindex+1,end);
+
+        return root;
+    }
+    
+}
